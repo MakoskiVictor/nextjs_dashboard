ec675971043383bcb15ee2273812dd9f8e6806ab
MakoskiVictor <makoski.ed@gmail.com>
2024-03-12 20:08:45 -0300

    fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,6 @@
 // BIBLIOTECA DE NEXTJS QUE TE PERMITE IMPORTAR FUENTES DE GOOGLE FONTS
-import { Montserrat } from "next/font/google"
+import { Montserrat, Lusitana } from "next/font/google"
 // CREO LA CONST PARA EXPORTAR AL LAYOUT
 export const montserrat = Montserrat({ subsets: ["latin"], weight: ["100","200","300","400","500","600","700"] })
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400","700"] })
